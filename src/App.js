68bf9dfd2939961c740680c594880844f3f8878f
EzatRashid <ezat.b.rashid@gmail.com>
2023-08-05 12:31:44 -0600

    finished movie app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React from "react";
-import { useEffect } from "react";
+import { useState,useEffect } from "react";
 import './app.css'
 import SearchIcon from'./search.svg'
+import MovieCard from "./MovieCard";
+
 
 
 const API_URL = 'http://www.omdbapi.com?apikey=bf69d59a'
 
+const movie1 = 
+    {
+        "Title": "Superman & Lois",
+        "Year": "2021–",
+        "imdbID": "tt11192306",
+        "Type": "series",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BOTA2MDVhMWItNTYwYi00OTcyLWJjZmEtNTQ2NTAxMDQyYTQwXkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_SX300.jpg"
+    }
+
 // bf69d59a
 const App = () =>{
+
+    const [movies, setMovies] = useState([])
+    const [searchTerm, setSearchTerm] = useState('')
 
     const searchMovies = async(title) =>{
         const responce = await fetch(`${API_URL}&s=${title}`)
         const data = await responce.json()
 
-        console.log(data.Search)
+        setMovies(data.Search)
 
     }   
     useEffect(() => {
@@ -26,19 +40,29 @@ const App = () =>{
         <div className="search">
             <input
             placeholder="Search for Movies"
-            value={'Superman'}
-            onChange={() => {}}
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
             />
 
             <img 
             src={SearchIcon}
             alt="Search"
-            onClick={() => {}}
+            onClick={() => searchMovies(searchTerm)}
             />
         </div>
-        <div className="container">
-        
-        </div>
+
+        {
+            movies?.length > 0 ?
+           ( <div className="container">
+            {movies.map((movie) => (
+                <MovieCard movie = {movie}/>
+            ))}
+              </div> ) : (
+                <div className="empty"><h2>No movies found</h2></div>
+              )
+        }
+
+       
      </div>
      )
 }
